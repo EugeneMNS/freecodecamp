35746c4c5adb015d165c7a12e5365d6580ae87a2
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-10-23 19:28:55 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -77,3 +77,5 @@ const myFirstName ="Bosch"
 const myLastName = "Roberto"
 
 var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
