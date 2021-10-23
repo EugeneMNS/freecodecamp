915221222da54f3f5af635f5df6d1a4a1c729a17
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-10-23 22:15:37 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -83,3 +83,6 @@ var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 var myStr = "This is the start. " + "This is the end."; // Change this line
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence."
+// Only change code below this line
+var myName = "Roberto";
+var myStr = "My name" + myName + "and I am well!";
