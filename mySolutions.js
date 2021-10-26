048880d3f35b03bafc161b39a4af33d21abbfb5b
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-10-26 21:32:51 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -151,3 +151,7 @@ var myArray = [18,64,99];
 
 // Only change code below this line
 myArray[0] = [45];
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
