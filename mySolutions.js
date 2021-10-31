456764d7cfda528f6d0215d340286257851ffb12
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-10-31 08:31:36 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -161,3 +161,10 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push (["dog", 3]);
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+
+let removedFromMyArray = myArray.pop();
