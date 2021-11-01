080e697209dcaa8b57e3db8c2b9edaf3aad59128
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-01 16:09:07 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -189,3 +189,8 @@ function reusableFunction() {
   console.log("Hi World");
 }
 reusableFunction ();
+
+function functionWithArgs(num1, num2){
+  console.log(num1 + num2);
+}
+functionWithArgs(7, 9);
