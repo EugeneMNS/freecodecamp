9554f8c99c2f726f396dc6c91bca7e1edab4cbb0
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-04 20:30:26 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -268,3 +268,10 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
