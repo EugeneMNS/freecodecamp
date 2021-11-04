d76caffc27f9bc32f3464e114105c6a9fd7e578c
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-04 21:39:21 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -317,3 +317,12 @@ function compareEquality(a, b) {
 }
 
 compareEquality(10, "10");
+// Setup
+function testNotEqual(val) {
+  if (val !=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
