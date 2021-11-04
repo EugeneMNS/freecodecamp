fbf9554a1e5427d5e4362cbf94949c172b365c07
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-04 22:09:07 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -377,3 +377,17 @@ testGreaterOrEqual(10);
 }
 
 testLessThan(10);
+
+function testLessOrEqual(val) {
+  if (val <=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
