f6d69f0e4cc4ba6b2861027702dd2bb899a71a1f
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-06 21:48:49 +0300

    Update mySolutions.js

--- mySolutions.js
+++ mySolutions.js
@@ -765,3 +765,21 @@ function checkObj(obj, checkProp) {
   return "Not Found";
   // Only change code above this line
 }
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+
+  arr.push(item);
+  var removed = arr.shift();
+  return removed; 
+}
+  // Only change code above this line
+
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
